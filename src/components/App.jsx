220bd90933229bab57f5bfc7673795e21c630f35
Jevaughn Vickers <114950498+jevaughnvick@users.noreply.github.com>
2025-05-03 13:40:22 -0500

    set up routing and add home page

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,65 +1,27 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "../pages/Home";
+import Layout from "./Layout";
+
+
+const router = createBrowserRouter([
+
+    {
+        path: "/",
+        element: <Layout />,
+        // errorElement: <NotFound />,
+        children: [
+            {index: true, element: <Home />},
+            // {path: "/game", element: <Shop />},
+        ]
+    }
+]);
+
+
 export default function App(){
 
     return (
         <>
-        <table className="w-full">
-            <thead className="text-3xl">
-                <tr>
-                    <td>
-                        Boy
-                    </td>
-                    <td>
-                        Girl
-                    </td>
-                    <td>
-                        Animal
-                    </td>
-                    <td>
-                        Place
-                    </td>
-                    <td>
-                        Food
-                    </td>
-                    <td>
-                        Thing
-                    </td>
-                    <td>
-                        TV/ Film
-                    </td>
-                    <td>
-                        Total
-                    </td>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>
-                        
-                    </td>
-                    <td>
-                        
-                    </td>
-                    <td>
-                        
-                    </td>
-                    <td>
-                        
-                    </td>
-                    <td>
-                        
-                    </td>
-                    <td>
-                        
-                    </td>
-                    <td>
-                        
-                    </td>
-                    <td>
-                        
-                    </td>
-                </tr>
-            </tbody>
-        </table>
+        <RouterProvider router={router} />
         </>
     )
 }
