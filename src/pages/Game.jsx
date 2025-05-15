a643fc584755be7e3802d301164ce0e15171de5a
Jevaughn Vickers <114950498+jevaughnvick@users.noreply.github.com>
2025-05-15 18:55:42 -0500

    start letter carousel

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -3,11 +3,14 @@ export default function Game(){
     return (
 
         <>
-        <div id="game" className="border border-red-500 h-full flex justify-center items-center">
-            <div id="letters" className="border border-red-500">
-                <div className="border border-yellow-500 w-[800px] h-[250px] flex justify-between bottom">
-                    <div className=""></div>
-                    <div></div>
+        <div id="game" className="h-full flex justify-center items-center">
+            <div id="letters" className="">
+                <div className="bottom">
+                    <div>A</div>
+                    <div>C</div>
+                    <div className="top">
+                        <div>B</div>
+                    </div>
                 </div>
             </div>
         </div>
